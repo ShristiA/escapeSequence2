0f3180a4ae9c276e7ec1640a139896cb774e0d40
ShristiA <shristiamatya3@gmail.com>
2018-10-04 10:53:48 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.print("Cost \t Quantity \t Total");
+	System.out.print("\n");
+	System.out.println("$1,000 \t     4 \t   $4000.00");
+	System.out.println("$  50.0 \t 8 \t    $400.00");
+	System.out.print("\n");
+	System.out.println("----------------------------");
+	System.out.print("Total:       12  \t$4,400.00");
+    }
+}
